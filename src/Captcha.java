42eb11d5980028ddeb6ba2e91e929d68f92f2f6d
pdmoore <pdmoore@gmail.com>
2017-12-01 23:02:13 -0800

    day 1, part 1

---
+++ src/Captcha.java
@@ -0,0 +1,16 @@
+public class Captcha {
+    public static int sum(String inputAsString) {
+        int captchaSum = 0;
+
+        int previousDigit = inputAsString.charAt(0);
+        for (int i = 1; i < inputAsString.length(); i++) {
+            if (inputAsString.charAt(i) == previousDigit) {
+                captchaSum += Character.digit(previousDigit, 10);
+            }
+            previousDigit = inputAsString.charAt(i);
+        }
+        if (inputAsString.charAt(inputAsString.length() - 1) == inputAsString.charAt(0)) captchaSum += Character.digit(inputAsString.charAt(0), 10);
+
+        return captchaSum;
+    }
+}
